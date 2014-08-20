79c7e49bca2155c16afab309c172751e7cce51bd
Donghwa Lee <dh09.lee@samsung.com>
2014-08-20 10:11:22 +0900

    drm: ipptest: support ipp dynamic color range
    This patch supports dynamic range for color space conversion.
    COLOR_RANGE_LIMITED -> selects narrow
    COLOR_RANGE_FULL    -> selects wide
    
    Change-Id: I94190ac3f3630904d2461e226d34378ac2b82b9d
    Signed-off-by: Donghwa Lee <dh09.lee@samsung.com>

--- exynos/exynos_drm.h
+++ exynos/exynos_drm.h
@@ -275,6 +275,11 @@ enum drm_exynos_ipp_cmd {
 	IPP_CMD_MAX,
 };
 
+enum drm_exynos_color_range {
+	COLOR_RANGE_LIMITED,
+	COLOR_RANGE_FULL,
+};
+
 /**
  * A structure for ipp property.
  *
@@ -290,6 +295,7 @@ struct drm_exynos_ipp_property {
 	__u32	ipp_id;
 	__u32	prop_id;
 	__u32	refresh_rate;
+	__u32	range;
 };
 
 /* definition of buffer */
