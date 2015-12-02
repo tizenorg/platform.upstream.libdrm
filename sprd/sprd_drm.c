2db791a36b630cc768a4ce3dca5c65d56c6f36a7
Roman Marchenko <r.marchenko@samsung.com>
2015-12-02 11:21:08 +0200

    [SPRD] Clears hooks and clears list of sprd devices
    Signed-off-by: Roman Marchenko <r.marchenko@samsung.com>
    
    Change-Id: I0311c80ec0f3d4660ddfd642294eb46ebcd36a1f

--- sprd/sprd_drm.c
+++ sprd/sprd_drm.c
@@ -1842,8 +1842,18 @@ err:
  */
 void sprd_device_destroy(struct sprd_drm_device *dev)
 {
-	//TODO::
 
+	//clear hooks
+	drmIoctlSetHook(NULL);
+	drmHandleEventSetHook(NULL);
+
+	//TODO::
+	//removing all resource,
+	//disabling overlay and connectors
+
+	DRMLISTDEL(&dev->link);
+
+	DRMINITLISTHEAD(&(devices));
 	free(dev);
 }
 
